f9ebe08835f8e0c17c315458ef2a75cce52c7a4c
ntr <ntr@ihfood.dk>
2017-04-28 13:20:12 +0200

    Add project files.

---
+++ DrawTextBenchmark/DrawText.cs
@@ -0,0 +1,202 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Horology;
+using BenchmarkDotNet.Jobs;
+
+namespace DrawTextBenchmark
+{
+    [Config(typeof(FastAndDirtyConfig))]
+    public class DrawText
+    {
+        public class FastAndDirtyConfig : ManualConfig
+        {
+            public FastAndDirtyConfig()
+            {
+                Add(new MemoryDiagnoser());
+                Add(Job.Default
+                    .WithLaunchCount(1)     // benchmark process will be launched only once
+                    .WithIterationTime(TimeInterval.FromMilliseconds(100)) // 100ms per iteration
+                    .WithWarmupCount(3)
+                    .WithTargetCount(10)
+                );
+            }
+        }
+
+        const float DPI = 96;
+        const int Width = 1024;
+        const int Height = 32;
+        const string Text = "This is a test text with numbers 0123456789 and symbols .,&%#";
+        const double FontSize = 16;
+        static readonly CultureInfo TextCultureInfo = CultureInfo.GetCultureInfo("en-us");
+        static readonly Typeface TextTypeface = new Typeface("Courier New");
+        //static readonly Pen TextShadowPen = new Pen(Brushes.Black, 3);
+        //static readonly Brush TextBackgroundBrush = new SolidColorBrush(Color.FromArgb(64, 0, 0, 0));
+        static readonly Brush TextForegroundBrush = Brushes.White;
+        static readonly Point TextOrigin = new Point(10, 5);
+
+        readonly GlyphTypeface glyphTypeface;
+        readonly IDictionary<int, ushort> characterToGlyphMap;
+        readonly IDictionary<ushort, double> advanceWidthsDictionary;
+        readonly Dictionary<char, ushort> characterToGlyphIndex;
+        readonly Dictionary<ushort, double> glyphIndexToAdvanceWidth;
+        readonly ushort[] glyphIndexes;
+        readonly double[] advanceWidths;
+        readonly GlyphRun glyphRun;
+
+        readonly DrawingVisual m_drawingVisual = new DrawingVisual();
+        readonly RenderTargetBitmap m_renderBitmap = new RenderTargetBitmap(Width, Height, DPI, DPI, PixelFormats.Default);
+
+        public DrawText()
+        {
+            if (!TextTypeface.TryGetGlyphTypeface(out glyphTypeface))
+            {
+                throw new ArgumentException("GlyphTypeface");
+            }
+            characterToGlyphMap = glyphTypeface.CharacterToGlyphMap;
+            advanceWidthsDictionary = glyphTypeface.AdvanceWidths;
+
+            var text = Text;
+            glyphIndexes = new ushort[text.Length];
+            advanceWidths = new double[text.Length];
+
+            var characterCount = characterToGlyphMap.Count;
+            characterToGlyphIndex = new Dictionary<char, ushort>(characterCount);
+            // Don't need dictionary for glyph index probably since it should be from 0-count
+            glyphIndexToAdvanceWidth = new Dictionary<ushort, double>(characterCount);
+
+            foreach (var kvp in characterToGlyphMap)
+            {
+                var c = (char)kvp.Key;
+                var glyphIndex = kvp.Value;
+                characterToGlyphIndex.Add(c, glyphIndex);
+
+                double width = advanceWidthsDictionary[glyphIndex] * FontSize;
+                if (!glyphIndexToAdvanceWidth.ContainsKey(glyphIndex))
+                {
+                    glyphIndexToAdvanceWidth.Add(glyphIndex, width);
+                }
+            }
+
+            double totalWidth = 0;
+            for (int n = 0; n < text.Length; n++)
+            {
+                var c = text[n];
+                ushort glyphIndex;
+                if (!characterToGlyphIndex.TryGetValue(c, out glyphIndex))
+                {
+                    glyphIndex = characterToGlyphMap[c];
+                    characterToGlyphIndex.Add(c, glyphIndex);
+                }
+                glyphIndexes[n] = glyphIndex;
+
+                double width;
+                if (!glyphIndexToAdvanceWidth.TryGetValue(glyphIndex, out width))
+                {
+                    width = advanceWidthsDictionary[glyphIndex] * FontSize;
+                    glyphIndexToAdvanceWidth.Add(glyphIndex, width);
+                }
+                advanceWidths[n] = width;
+
+                totalWidth += width;
+            }
+            glyphRun = new GlyphRun(glyphTypeface, 0, false, FontSize, DPI,
+                glyphIndexes, TextOrigin, advanceWidths, null, null, null, null,
+                null, null);
+        }
+
+        [Benchmark]
+        public void RenderOpenDispose()
+        {
+            using (var dc = m_drawingVisual.RenderOpen())
+            {
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public void DrawFormattedText()
+        {
+            using (var dc = m_drawingVisual.RenderOpen())
+            {
+                var formattedText = new FormattedText(Text, TextCultureInfo,
+                    FlowDirection.LeftToRight, TextTypeface, FontSize, TextForegroundBrush, DPI);
+                dc.DrawText(formattedText, TextOrigin);
+            }
+        }
+
+        public void DrawFormattedText_Save()
+        {
+            m_renderBitmap.Clear();
+            DrawFormattedText();
+            m_renderBitmap.Render(m_drawingVisual);
+            SaveBitmap(m_renderBitmap);
+        }
+
+        [Benchmark]
+        public void DrawGlyphRun()
+        {
+            using (var dc = m_drawingVisual.RenderOpen())
+            {
+                var text = Text;
+                //double totalWidth = 0;
+                for (int n = 0; n < text.Length; n++)
+                {
+                    var c = text[n];
+                    ushort glyphIndex = characterToGlyphIndex[c];
+                    //ushort glyphIndex;
+                    //if (!characterToGlyphIndex.TryGetValue(c, out glyphIndex))
+                    //{
+                    //    glyphIndex = characterToGlyphMap[c];
+                    //    characterToGlyphIndex.Add(c, glyphIndex);
+                    //}
+                    glyphIndexes[n] = glyphIndex;
+
+                    double width = glyphIndexToAdvanceWidth[glyphIndex];
+                    //double width;
+                    //if (!glyphIndexToAdvanceWidth.TryGetValue(glyphIndex, out width))
+                    //{
+                    //    width = advanceWidthsDictionary[glyphIndex] * FontSize;
+                    //    glyphIndexToAdvanceWidth.Add(glyphIndex, width);
+                    //}
+                    advanceWidths[n] = width;
+
+                    //totalWidth += width;
+                }
+                double y = TextOrigin.Y + Math.Round(glyphTypeface.Baseline * FontSize);
+                var origin = new Point(TextOrigin.X, y);
+
+                var glyphRun = new GlyphRun(glyphTypeface, 0, false, FontSize, DPI,
+                    glyphIndexes, origin, advanceWidths, null, null, null, null,
+                    null, null);
+                dc.DrawGlyphRun(TextForegroundBrush, glyphRun);
+            }
+        }
+
+        public void DrawGlyphRun_Save()
+        {
+            m_renderBitmap.Clear();
+            DrawGlyphRun();
+            m_renderBitmap.Render(m_drawingVisual);
+            SaveBitmap(m_renderBitmap);
+        }
+
+        private static void SaveBitmap(BitmapSource bitmap, [CallerMemberName] string fileName = null)
+        {
+            using (Stream stream = new FileStream(fileName + ".png", FileMode.Create))
+            {
+                var encoder = new PngBitmapEncoder();
+                encoder.Interlace = PngInterlaceOption.Off;
+                encoder.Frames.Add(BitmapFrame.Create(bitmap));
+                encoder.Save(stream);
+            }
+        }
+    }
+}
---
+++ DrawTextBenchmark/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Running;
+
+namespace DrawTextBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var summary = BenchmarkRunner.Run<DrawText>();
+
+            var drawText = new DrawText();
+            drawText.DrawFormattedText_Save();
+            drawText.DrawGlyphRun_Save();
+            //for (int i = 0; i < 1000000; i++)
+            //{
+            //    drawText.DrawGlyphRun();
+            //}
+        }
+    }
+}
